8e45c548656dc619ec6a97d5f78f5ad59d35165b
kanirya <danishkanirya9999@gmail.com>
2026-04-03 17:45:01 +0500

    adding packages

--- packages/api-client/tsup.config.ts
+++ packages/api-client/tsup.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
   dts: true,
   sourcemap: true,
   clean: true,
-  external: ['react', '@tanstack/react-query'],
+  external: ['react', '@tanstack/react-query', '@edupilot/types'],
 });
